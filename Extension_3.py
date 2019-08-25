3de9b23e079645ceef672343b9067888d8f453ce
RyosukeTomura <54223589+RyosukeTomura@users.noreply.github.com>
2019-08-25 16:33:22 +1000

    Add files via upload

---
+++ Extension_3.py
@@ -0,0 +1,26 @@
+word_list = []
+word = input("Enter a word:")
+while word != "quit":
+    word = input("Enter a word, or quit: ")
+
+if word != "quit":
+    word_list.append(word)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
